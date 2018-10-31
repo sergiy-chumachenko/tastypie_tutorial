0a6c305f5388443c6823c9ff98ae362277bcb14f
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2018-10-31 15:48:14 +0200

    [INIT] Repo Initialization

---
+++ services/api.py
@@ -0,0 +1,9 @@
+# # services/api.py
+# from tastypie.resources import ModelResource
+# from services.models import Entry
+#
+#
+# class EntryResource(ModelResource):
+#     class Meta:
+#         queryset = Entry.objects.all()
+#         resource_name = 'entry'
---
+++ services/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+import uuid
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    product_type = models.CharField(max_length=50)
+    price = models.IntegerField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.id
+
